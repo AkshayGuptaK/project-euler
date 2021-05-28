e6756b1f628e838b4296e9a2ce21527c028e58aa
Akshay Gupta <akshaygrkesh@gmail.com>
2021-05-28 13:53:58 +0530

    Optimize soln for problem 1

--- src/project_euler/problems/1.clj
+++ src/project_euler/problems/1.clj
@@ -1,12 +1,14 @@
-(ns project-euler.problems.1)
+(ns project-euler.problems.1
+  (:require [project-euler.utils :as utils]))
 
 ;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 ;; The sum of these multiples is 23.
 ;; Find the sum of all the multiples of 3 or 5 below 1000.
 
-(defn- is-not-a-multiple-of-n? [n x] (not (= 0 (mod x n))))
-
-(defn- sum-of-non-multiples [n & multiples]
-  (transduce (filter (apply every-pred (map #(partial is-not-a-multiple-of-n? %) multiples))) + (range n)))
+(defn- sum-of-non-multiples [n m1 m2]
+  (-> n
+      utils/sum-to-n
+      (+ (utils/sum-to-n n (* m1 m2)))
+      (- (utils/sum-to-n n m1) (utils/sum-to-n n m2))))
 
 (def solution (time (sum-of-non-multiples 1000 3 5)))
