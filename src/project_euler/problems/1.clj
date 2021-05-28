d8f70236eeb17a32d39d5cc309ea297a385276d9
Akshay Gupta <akshaygrkesh@gmail.com>
2021-05-28 11:45:05 +0530

    Add problem statements and docstrings

--- src/project_euler/problems/1.clj
+++ src/project_euler/problems/1.clj
@@ -1,5 +1,9 @@
 (ns project-euler.problems.1)
 
+;; If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+;; The sum of these multiples is 23.
+;; Find the sum of all the multiples of 3 or 5 below 1000.
+
 (defn- is-not-a-multiple-of-n? [n x] (not (= 0 (mod x n))))
 
 (defn- sum-of-non-multiples [n & multiples]
--- src/project_euler/problems/3.clj
+++ src/project_euler/problems/3.clj
@@ -1,7 +1,12 @@
 (ns project-euler.problems.3
   (:require [clojure.math.numeric-tower :as numeric]))
 
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;; What is the largest prime factor of the number 600851475143 ?
+
 (defn- smallest-factor [n]
+  "Returns the smallest non-trivial prime factor of `n`.
+   If `n` is prime it returns nil."
   (->> n
        numeric/exact-integer-sqrt
        first
@@ -10,6 +15,8 @@
        first))
 
 (defn- prime-factorization [num]
+  "Returns all the non-trivial prime factors of `num`.
+   If `num` is prime it returns [`num`]."
   (loop [n num
          factors []]
     (if-let [factor (smallest-factor n) ]
