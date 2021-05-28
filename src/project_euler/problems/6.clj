313d9c2a4cd8dfc25dffe630f12b10f4f976075d
Akshay Gupta <akshaygrkesh@gmail.com>
2021-05-28 22:57:32 +0530

    Add problem prompts

--- src/project_euler/problems/6.clj
+++ src/project_euler/problems/6.clj
@@ -2,6 +2,13 @@
   (:require [clojure.math.numeric-tower :as numeric]
             [project-euler.utils :as utils]))
 
+;; The sum of the squares of the first ten natural numbers is 385.
+;; The square of the sum of the first ten natural numbers is 3025.
+;; Hence the difference between the sum of the squares of the
+;; first ten natural numbers and the square of the sum is 2640.
+;; Find the difference between the sum of the squares of the
+;; first one hundred natural numbers and the square of the sum.
+
 (defn- difference-of-squares
   "Returns the difference between the square of the sum to `n` and the sum of the first `n` squares."
   [n]
--- src/project_euler/problems/7.clj
+++ src/project_euler/problems/7.clj
@@ -1,6 +1,9 @@
 (ns project-euler.problems.7
   (:require [project-euler.utils :as utils]))
 
+;; By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+;; What is the 10001st prime number?
+
 (defn- nth-prime
   "Returns the nth prime number"
   [n]
