1f8dbc09796bd025ced24bef1426a54e8ba06b34
Akshay Gupta <akshaygrkesh@gmail.com>
2021-06-26 13:56:31 +0530

    Refactor Problem 3

--- src/project_euler/problems/3.clj
+++ src/project_euler/problems/3.clj
@@ -6,13 +6,10 @@
 ;; What is the largest prime factor of the number 600851475143 ?
 
 (defn- smallest-factor
-  "Returns the smallest non-trivial prime factor of `n`.
+  "Returns the smallest non-trivial prime factor of `n` between `smallest` and `largest`.
    If `n` is prime it returns nil."
-  [n]
-  (->> n
-       numeric/exact-integer-sqrt
-       first
-       (range 2)
+  [n smallest largest]
+  (->> (range smallest largest)
        (filter (partial utils/divides? n))
        first))
 
@@ -20,10 +17,12 @@
   "Returns all the non-trivial prime factors of `num`.
    If `num` is prime it returns [`num`]."
   [num]
-  (loop [n num
-         factors []]
-    (if-let [factor (smallest-factor n) ]
-      (recur (/ n factor) (conj factors factor))
-      (conj factors n))))
+  (let [largest-factor (/ num 2)]
+    (loop [n num
+           factor 2
+           factors []]
+      (if-let [factor (smallest-factor n factor largest-factor)]
+        (recur (/ n factor) factor (conj factors factor))
+        (conj factors n)))))
 
 (def solution (time (apply max (prime-factorization 600851475143))))
