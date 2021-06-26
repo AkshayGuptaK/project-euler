8015a81928845ed607b6b539f485df8b7b569b05
Akshay Gupta <akshaygrkesh@gmail.com>
2021-06-26 13:58:54 +0530

    Refactor Problem 5

--- src/project_euler/problems/5.clj
+++ src/project_euler/problems/5.clj
@@ -1,6 +1,5 @@
 (ns project-euler.problems.5
-  (:require [clojure.math.numeric-tower :as numeric]
-            [project-euler.utils.prime :as prime]))
+  (:require [project-euler.utils.prime :as prime]))
 
 ;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 ;; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
@@ -8,11 +7,10 @@
 (defn- highest-power-up-to
   "Returns the highest power of `num` whose value is less than or equal to `limit`"
   [limit num]
-  (loop [n num
-         exp 1]
-    (if (> (numeric/expt n exp) limit)
-      (numeric/expt n (dec exp))
-      (recur n (inc exp)))))
+  (->> num
+       (iterate #(* num %))
+       (take-while #(<= % limit))
+       last))
 
 (defn- smallest-evenly-divisible-by-all-up-to
   "Returns the smallest positive number that is evenly divisible by all numbers up to and including `limit`"
